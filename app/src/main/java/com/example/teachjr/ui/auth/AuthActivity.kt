64ee00707cca37537247fd618b0ac9cc21730af9
Abizer-R <rabizeridris@gmail.com>
2022-10-12 12:43:19 +0530

    Designed and added Fragments. Logic Implementation pending

--- app/src/main/java/com/example/teachjr/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/example/teachjr/ui/auth/AuthActivity.kt
@@ -3,12 +3,19 @@ package com.example.teachjr.ui.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.teachjr.R
+import com.example.teachjr.databinding.ActivityAuthBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        
     }
 }
